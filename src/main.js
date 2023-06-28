09a0ec95a143dc685ca106dd77b2de94ffad2086
Geovanna Mayra Domingos do Nascimento <gmdomingosn@gmail.com>
2023-06-28 12:23:10 -0300

    adiciona comentário e muda linguagem do html

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// O createApp é a função do vue que vai possibilitar a manipulação da nossa página html por meio de vue. neste caso, há uam função mount que vai montar no html com id "app" o App.vue importado e criado pela função do vue createApp 
